f524eb363330349f46a8d4d7630ae29f1c3d6d8b
ollia008 <97535630+ollia008@users.noreply.github.com>
2024-12-18 11:26:57 +0100

    Added new user functionality

---
+++ Nettbutikk/Controllers/NewUserController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace Nettbutikk.Controllers
+{
+    public class NewUserController : Controller
+    {
+        public IActionResult NewUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult NewUser(LoginnModel objLoginnModel)
+        {
+            string conStr = "Data Source=.\\SQLEXPRESS; Initial Catalog=nettbutikk; Integrated Security=True";
+            SqlConnection con = new SqlConnection(conStr);
+            string bruker = objLoginnModel.Username;
+            string passord = objLoginnModel.Password;
+            string qry = $"insert into LoginInfo values ('{bruker}', '{passord}')";
+            con.Open();
+            SqlCommand cmd = new SqlCommand(qry, con);
+            cmd.ExecuteNonQuery();
+
+            return View();
+        }
+    }
+}
